9b9d2a0e3a10c9effa5e55ec0fec7cb4d5b24d71
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2024-12-15 22:58:20 +0530

    Stream Code and More Notes

--- src/Java8/Streams.java
+++ src/Java8/Streams.java
@@ -1,4 +1,23 @@
 package Java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class Streams {
+    public static void main(String[] args) {
+        //Add numbers
+//        List<Integer> list = Arrays.asList(5,4,6);
+//      Optional<Integer> sum = list.stream().reduce((a,b)->a+b);
+//        System.out.println(sum.get());
+
+        //print average of numbers
+
+        List<Integer> list = Arrays.asList(1,2,5,5);
+        double avg = list.stream().mapToInt(e->e).average().getAsDouble();
+        System.out.println(avg);
+
+
+
+    }
 }
