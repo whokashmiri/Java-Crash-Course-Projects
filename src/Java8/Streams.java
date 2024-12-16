76d78b467622a2d40d0cab39beb01e63a64c0b4e
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2024-12-16 12:54:01 +0530

    Stream And Notes Added

--- src/Java8/Streams.java
+++ src/Java8/Streams.java
@@ -3,6 +3,7 @@ package Java8;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Streams {
     public static void main(String[] args) {
@@ -12,10 +13,19 @@ public class Streams {
 //        System.out.println(sum.get());
 
         //print average of numbers
+//
+//        List<Integer> list = Arrays.asList(1,2,5,5);
+//        double avg = list.stream().mapToInt(e->e).average().getAsDouble();
+//        System.out.println(avg);
 
-        List<Integer> list = Arrays.asList(1,2,5,5);
-        double avg = list.stream().mapToInt(e->e).average().getAsDouble();
-        System.out.println(avg);
+//        List<Integer> list = Arrays.asList(10,50,5,4,50);
+//        double avg =list.stream().map(e->e*e).filter(e->e>100).mapToInt(e -> e).average().getAsDouble();
+//        System.out.println(avg);
+
+        List<Integer> list = Arrays.asList(54,26,51,-26,245,256);
+       List<Integer> list1 = list.stream().map(e->String.valueOf(e)).filter(e->e.startsWith("2"))
+                .map(Integer::valueOf).collect(Collectors.toList());
+        System.out.println(list1);
 
 
 
