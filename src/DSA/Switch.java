4fa0e78e25f05dff19ea85b41fc6798b752a1579
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2024-12-17 22:32:06 +0530

    Switch

---
+++ src/DSA/Switch.java
@@ -0,0 +1,44 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String fr = scanner.next();
+       String fruit = fr.toLowerCase();
+        switch (fruit){
+            case "mango":
+                System.out.println(fruit);
+                break;
+            case "apple":
+                System.out.println(fruit);
+                break;
+            case "apricot":
+                System.out.println(fruit);
+                break;
+            default:
+                System.out.println("No Case");
+        }
+
+
+        switch (fruit){
+            case "mango":
+            case "apple":
+            case "apricot":
+                System.out.println(fruit);
+                break;
+            default:
+                System.out.println("No Case");
+        }
+
+
+        switch (fruit) {
+            case "mango"-> System.out.println("Mango");
+            case "apple" -> System.out.println("Apple");
+            case "apricot" -> System.out.println("Apricot");
+            default ->   System.out.println("No Case");
+
+        }
+    }
+}
