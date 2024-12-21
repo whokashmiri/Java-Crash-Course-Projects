c9e514734d8db497e0e0999cd169ffad8ca89e10
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2024-12-21 22:56:24 +0530

    Binary Search

---
+++ src/DSA/Algorithm/BinarySearch.java
@@ -0,0 +1,25 @@
+package DSA.Algorithm;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int [] arr = {-2 ,2,3,4,55 ,68,79,80};
+        System.out.println( biSearch(arr ,79 ));
+
+    }
+    static int biSearch(int[] arr, int target){
+        int start = 0;
+        int end  = arr.length-1;
+        while(start<=end){
+            int mid = start +(end-start)/2;
+            if(target<arr[mid]){
+                end = mid-1;
+            } else if (target> arr[mid]) {
+                start = mid + 1;
+            }else {
+                return  mid;
+            }
+        }
+
+        return -1;
+    }
+}
