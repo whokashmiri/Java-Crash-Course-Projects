d4e30801e69de7aadaf1734a2828e029ce02d3e4
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2024-12-31 22:41:40 +0530

    Selection Sort

---
+++ src/DSA/Algorithm/SelectionSort.java
@@ -0,0 +1,36 @@
+package DSA.Algorithm;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int  []arr = {-3,5,9,10,8,4};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static  void selectionSort(int [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length-i-1;
+            int maxIdx = getMaxIndex(arr, 0 , last);
+            swap(arr,maxIdx ,last);
+
+        }
+    }
+    static void swap(int [] arr, int first , int second){
+        int temp = arr[first];
+        arr[first]= arr[second];
+        arr[second]= temp;
+    }
+
+    private static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <=end ; i++) {
+            if (arr[max]<arr[i]){
+                max = i;
+            }
+
+        }
+        return max;
+    }
+}
