f17f8572b88d6562dcaf77e4b3ebb905e4096f6a
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-01-09 22:29:59 +0530

    Strings

---
+++ src/Strings/StringOne.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class StringOne {
+    public static void main(String[] args) {
+        String string="Aaqib";
+        System.out.println(string);
+    }
+}
---
+++ src/Strings/StringTwo.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class StringTwo {
+    public static void main(String[] args) {
+        String a = "aaqib";
+        String b = new String("aaqib");
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+        System.out.println(56);
+        System.out.println("hello");
+        System.out.println(new int[] {4,3,5});
+        String name = null;
+        System.out.println(name);
+    }
+}
