88db722f0142a8936149ede752f377431708a2a2
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-01-10 22:50:57 +0530

    String Continue

---
+++ src/Strings/StringFour.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class StringFour {
+    public static void main(String[] args) {
+//        String str = "";
+        for (int i = 0; i <26 ; i++) {
+            char ch = (char) ('a' + i);
+            char chh = (char) ('A' + i);
+            System.out.println(ch);
+            System.out.println(chh);
+        }
+    }
+}
---
+++ src/Strings/StringThree.java
@@ -0,0 +1,21 @@
+package Strings;
+
+import java.util.ArrayList;
+
+public class StringThree {
+    public static void main(String[] args) {
+        //Pretty Printing
+//        float a  = 24.32432f;
+//        System.out.printf("Format %.2f", a);
+//        System.out.printf("%.3f",Math.PI);
+        System.out.println('q' + 'b');
+        System.out.println('q' - 'b');
+        System.out.println("a" + "b");
+        System.out.println((char) ('a' + 4));
+        System.out.println ('a' + 4);
+        System.out.println ('a' - 4);
+        System.out.println("a" + 4);
+        String ans  =  new Integer(98)+" "+new ArrayList<>();
+        System.out.println(ans);
+    }
+}
