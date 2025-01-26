5adf16d01c8ca4ddad21a61d8ade21f4a90e30ef
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-01-26 08:10:36 +0530

    fibonocci

--- src/DSA/Recursion/RecursionOne.java
+++ src/DSA/Recursion/RecursionOne.java
@@ -1,5 +1,14 @@
 package DSA.Recursion;
 
 public class RecursionOne {
-    
+    public static void main(String[] args) {
+        System.out.println(fib(4));
+
+    }
+    static  int fib(int n){
+        if(n < 2){
+            return n;
+        }
+        return fib(n-1) + fib (n-2);
+    }
 }
